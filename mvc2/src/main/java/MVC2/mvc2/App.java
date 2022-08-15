3005208eecb6920d624a67cd0e6887f4ecf72f2a
FelipeGomezGarcia <feli.goga@gmail.com>
2022-08-15 20:33:48 +0200

    Proyecto 2

---
+++ mvc2/src/main/java/MVC2/mvc2/App.java
@@ -0,0 +1,13 @@
+package MVC2.mvc2;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
---
+++ mvc2/src/test/java/MVC2/mvc2/AppTest.java
@@ -0,0 +1,18 @@
+package MVC2.mvc2;
+
+
+
+/**
+ * Unit test for simple App.
+ */
+public class AppTest 
+{
+    /**
+     * Create the test case
+     *
+     * @param testName name of the test case
+     */
+    public AppTest( String testName )
+    {
+    }
+}
