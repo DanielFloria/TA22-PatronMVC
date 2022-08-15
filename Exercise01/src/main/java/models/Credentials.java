bba65cf3ef02e2cd529aada0d40ad283aa906818
FelipeGomezGarcia <feli.goga@gmail.com>
2022-08-15 19:33:24 +0200

    DOCUMENTO DE CREDENCIALES

---
+++ Exercise01/src/main/java/models/Credentials.java
@@ -0,0 +1,9 @@
+package models;
+
+public class Credentials {
+	public static final String LOCAL_SERVER = "jdbc:mysql://IP_server";
+	public static final String LOCAL_USER = "USER";
+	public static final String LOCAL_PASSWORD = "PASSWORD";
+	
+	public static final String DATABASE_NAME = "DATABASE";
+}
