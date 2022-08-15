d6f1e411820996d2dc579e472cb3b16cf6052c64
FelipeGomezGarcia <feli.goga@gmail.com>
2022-08-15 20:29:06 +0200

    CREDENCIALES

--- Exercise01/src/main/java/models/Credentials.java
+++ Exercise01/src/main/java/models/Credentials.java
@@ -1,9 +1,9 @@
 package models;
 
 public class Credentials {
-	public static final String LOCAL_SERVER = "jdbc:mysql://192.168.0.106:3306";
-	public static final String LOCAL_USER = "remote";
-	public static final String LOCAL_PASSWORD = "Admin1234";
+	public static final String LOCAL_SERVER = "jdbc:mysql://IP_SERVER";
+	public static final String LOCAL_USER = "USER";
+	public static final String LOCAL_PASSWORD = "PASSWORD";
 	
 	public static final String DATABASE_NAME = "UD2201";
 }
