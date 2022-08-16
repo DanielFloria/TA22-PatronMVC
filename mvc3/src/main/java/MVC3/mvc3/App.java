9edc587006ed942ef8238ab9cd4490390bcb64cf
FelipeGomezGarcia <feli.goga@gmail.com>
2022-08-16 09:59:33 +0200

    MVC 3

---
+++ mvc3/src/main/java/MVC3/mvc3/App.java
@@ -0,0 +1,13 @@
+package MVC3.mvc3;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
---
+++ mvc3/src/test/java/MVC3/mvc3/AppTest.java
@@ -0,0 +1,18 @@
+package MVC3.mvc3;
+
+
+/**
+ * Unit test for simple App.
+ */
+public class AppTest 
+{
+    /**
+     * Create the test case
+     *
+     * @param testName name of the test case
+     */
+    public AppTest( String testName )
+    {
+    }
+
+}
